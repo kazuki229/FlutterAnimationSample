a4e189191cf0b05cce2c73f3d9e9c1d4a5f8b3ab
kazuki229 <tsuzuku.k@gmail.com>
2018-08-21 02:01:12 +0900

    :+1: add AnimatedWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
+class AnimatedLogo extends AnimatedWidget {
+  AnimatedLogo({Key key, Animation<double> animation})
+      : super(key: key, listenable: animation);
+
+  Widget build(BuildContext context) {
+    final Animation<double> animation = listenable;
+    return Center(
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10.0),
+        height: animation.value,
+        width: animation.value,
+        child: FlutterLogo(),
+      ),
+    );
+  }
+}
+
 class LogoApp extends StatefulWidget {
   _LogoAppState createState() => _LogoAppState();
 }
@@ -14,22 +31,11 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
     controller = AnimationController(
       duration: const Duration(milliseconds: 2000), vsync: this);
     animation = Tween(begin: 0.0, end: 300.0).animate(controller)
-    ..addListener(() {
-      setState(() {
-
-      });
-    });
     controller.forward();
   }
+
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 10.0),
-        height: animation.value,
-        width: animation.value,
-        child: FlutterLogo(),
-      ),
-    );
+    return AnimatedLogo(animation: animation);
   }
   dispose() {
     controller.dispose();
