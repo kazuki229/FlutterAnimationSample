27f74b72a4462bbdf0c34b42aa1f3ea4941c105c
kazuki229 <tsuzuku.k@gmail.com>
2018-08-21 01:46:41 +0900

    :art: add zoom up animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,39 @@
+import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
 class LogoApp extends StatefulWidget {
   _LogoAppState createState() => _LogoAppState();
 }
 
-class _LogoAppState extends State<LogoApp> {
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController controller;
+
+  initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: const Duration(milliseconds: 2000), vsync: this);
+    animation = Tween(begin: 0.0, end: 300.0).animate(controller)
+    ..addListener(() {
+      setState(() {
+
+      });
+    });
+    controller.forward();
+  }
   Widget build(BuildContext context) {
     return Center(
       child: Container(
         margin: EdgeInsets.symmetric(vertical: 10.0),
-        height: 300.0,
-        width: 300.0,
+        height: animation.value,
+        width: animation.value,
         child: FlutterLogo(),
       ),
     );
+  }
+  dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
 
